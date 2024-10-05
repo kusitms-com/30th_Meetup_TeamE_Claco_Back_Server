c6a1a14e069d3669567e7787026e4ed79d66e3ee
Keon <lune0410k@naver.com>
2024-10-05 17:33:38 +0900

    initial commit

---
+++ src/main/java/com/curateme/claco/ClacoApplication.java
@@ -0,0 +1,13 @@
+package com.curateme.claco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curateme/claco/ClacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curateme.claco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
