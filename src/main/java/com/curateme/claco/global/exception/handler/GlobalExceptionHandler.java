3f9bf28f2bccd7fad1e9297e6a34a9ec0c8114a8
Keon <lune0410k@naver.com>
2024-10-17 23:19:58 +0900

    feat: edit global handler and add ApiStatus

--- src/main/java/com/curateme/claco/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/curateme/claco/global/exception/handler/GlobalExceptionHandler.java
@@ -31,7 +31,7 @@ public class GlobalExceptionHandler {
 		log.error("[Exception] -> message: {}", exception.getMessage());
 		log.debug("[Exception] ->", exception);
 
-		return ApiResponse.fail(ApiStatus.EXCEPTION_OCCUR.getCode(), exception.getMessage());
+		return ApiResponse.fail(ApiStatus.EXCEPTION_OCCUR);
 	}
 
 	// TODO: 예외의 메시지 반환 부분 변경 (RuntimeException)
@@ -41,7 +41,7 @@ public class GlobalExceptionHandler {
 		log.error("[RuntimeException] -> message: {}", runtimeException.getMessage());
 		log.debug("[RuntimeException] ->", runtimeException);
 
-		return ApiResponse.fail(ApiStatus.RUNTIME_EXCEPTION_OCCUR.getCode(), runtimeException.getMessage());
+		return ApiResponse.fail(ApiStatus.RUNTIME_EXCEPTION_OCCUR);
 	}
 
 	@ExceptionHandler(value = BusinessException.class)
--- src/main/java/com/curateme/claco/global/response/ApiStatus.java
+++ src/main/java/com/curateme/claco/global/response/ApiStatus.java
@@ -16,6 +16,7 @@ import lombok.Getter;
  * DATE               AUTHOR        NOTE
  * -----------------------------------------------------------
  * 	2024.10.14   	   이 건        최초 생성
+ * 	2024.10.15		   이 건		   로그인 및 멤버 관련 에러 추가
  */
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -26,7 +27,16 @@ public enum ApiStatus {
 
 	// 서버 에러
 	EXCEPTION_OCCUR(HttpStatus.INTERNAL_SERVER_ERROR, "DBG-500", "Something went wrong."),
-	RUNTIME_EXCEPTION_OCCUR(HttpStatus.INTERNAL_SERVER_ERROR, "DBG-501", "Something went wrong.")
+	RUNTIME_EXCEPTION_OCCUR(HttpStatus.INTERNAL_SERVER_ERROR, "DBG-501", "Something went wrong."),
+
+	// 멤버 에러
+	MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEM-001", "Member not found."),
+
+	// 로그인 에러
+	ACCESS_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "ACT-001", "AccessToken not found."),
+	REFRESH_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "RFT-001", "RefreshToken not found."),
+	MEMBER_LOGIN_SESSION_EXPIRED(HttpStatus.BAD_REQUEST, "MSE-001", "Member login session expired."),
+	OAUTH_ATTRIBUTE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "ATH-001", "Cannot find OAuth attribute.")
 	;
 
 	private final HttpStatus httpStatus;
