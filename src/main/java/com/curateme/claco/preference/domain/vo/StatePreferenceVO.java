f0716e34e50ad6755d8eab2efff3af4bc3bd49ca
Keon <lune0410k@naver.com>
2024-10-18 22:35:53 +0900

    feat: add VO for preference

---
+++ src/main/java/com/curateme/claco/preference/domain/vo/StatePreferenceVO.java
@@ -0,0 +1,15 @@
+package com.curateme.claco.preference.domain.vo;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StatePreferenceVO {
+
+	private String preferenceState;
+
+}
