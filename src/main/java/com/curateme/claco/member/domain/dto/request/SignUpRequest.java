0d2cecfe37eaf0e8f41fd351020cf2f24433563e
Keon <lune0410k@naver.com>
2024-10-22 22:42:20 +0900

    feat: add validate conditions

--- src/main/java/com/curateme/claco/member/domain/dto/request/SignUpRequest.java
+++ src/main/java/com/curateme/claco/member/domain/dto/request/SignUpRequest.java
@@ -5,8 +5,9 @@ import java.util.List;
 import com.curateme.claco.member.domain.entity.Gender;
 import com.curateme.claco.preference.domain.vo.CategoryPreferenceVO;
 import com.curateme.claco.preference.domain.vo.RegionPreferenceVO;
-import com.curateme.claco.preference.domain.vo.StatePreferenceVO;
+import com.curateme.claco.preference.domain.vo.TypePreferenceVO;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,12 +20,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignUpRequest {
 
+	@NotNull
 	private String nickname;
+	@NotNull
 	private Gender gender;
+	@NotNull
+	private Integer age;
+	@NotNull
 	private Integer minPrice;
+	@NotNull
 	private Integer maxPrice;
 	private List<RegionPreferenceVO> regionPreferences;
-	private List<StatePreferenceVO> statePreferences;
+	private List<TypePreferenceVO> typePreferences;
 	private List<CategoryPreferenceVO> categoryPreferences;
 
 }
