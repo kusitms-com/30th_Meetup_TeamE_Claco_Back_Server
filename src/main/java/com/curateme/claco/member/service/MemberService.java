d1d69f949d22d70534c514d37216094b46186dd0
Keon <lune0410k@naver.com>
2024-10-22 22:51:56 +0900

    refactor: change return type to void

--- src/main/java/com/curateme/claco/member/service/MemberService.java
+++ src/main/java/com/curateme/claco/member/service/MemberService.java
@@ -1,7 +1,6 @@
 package com.curateme.claco.member.service;
 
 import com.curateme.claco.member.domain.dto.request.SignUpRequest;
-import com.curateme.claco.member.domain.dto.response.NicknameValidResponse;
 
 /**
  * @author      : 이 건
@@ -11,21 +10,20 @@ import com.curateme.claco.member.domain.dto.response.NicknameValidResponse;
  * DATE               AUTHOR        NOTE
  * -----------------------------------------------------------
  * 2024.10.18   	   이 건        최초 생성
+ * 2024.10.22   	   이 건        메서드 반환 타입 void로 변경(예외 활용에 따라)
  */
 public interface MemberService {
 
 	/**
 	 * 닉네임 유효성 체크 (중복 검사)
 	 * @param nickname : 검사하고자 하는 닉네임
-	 * @return : True=사용 가능, False=사용 불가
 	 */
-	NicknameValidResponse checkNicknameValid(String nickname);
+	void checkNicknameValid(String nickname);
 
 	/**
 	 * 취향 정보, 닉네임 정보를 받아와 회원가입을 완료
 	 * @param signUpRequest: nickname, gender, price, preference 정보
-	 * @return : void (응답 코드만)
 	 */
-	Void signUp(SignUpRequest signUpRequest);
+	void signUp(SignUpRequest signUpRequest);
 
 }
