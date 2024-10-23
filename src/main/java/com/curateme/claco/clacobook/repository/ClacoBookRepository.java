cf60d0b5501f5fa8f5b127262584cdbae49f32ad
Keon <lune0410k@naver.com>
2024-10-24 00:51:09 +0900

    docs: improve javadoc

--- src/main/java/com/curateme/claco/clacobook/repository/ClacoBookRepository.java
+++ src/main/java/com/curateme/claco/clacobook/repository/ClacoBookRepository.java
@@ -4,5 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.curateme.claco.clacobook.domain.entity.ClacoBook;
 
+/**
+ * @author      : 이 건
+ * @date        : 2024.10.24
+ * @author devkeon(devkeon123@gmail.com)
+ * ===========================================================
+ * DATE               AUTHOR        NOTE
+ * -----------------------------------------------------------
+ * 2024.10.24   	   이 건        최초 생성
+ */
 public interface ClacoBookRepository extends JpaRepository<ClacoBook, Long> {
 }
