1b23af0223d658cfdf2d0346f777e599205248e1
정희찬 <anselmo228@naver.com>
2024-10-31 13:13:42 +0900

    feature: Swagger Config Settings

---
+++ src/main/java/com/curateme/claco/global/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.curateme.claco.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        String jwtSchemeName = "JWT TOKEN";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+            .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                .name(jwtSchemeName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+
+        return new OpenAPI()
+            .addServersItem(new Server().url("/"))
+            .info(apiInfo())
+            .addSecurityItem(securityRequirement)
+            .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Calco Springdoc")
+            .description("Springdoc을 사용한 CLACO API 테스트")
+            .version("1.0.0");
+    }
+}
+
