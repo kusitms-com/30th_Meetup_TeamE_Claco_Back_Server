f0f93745b65286b4798cd4be1203622d4476bd47
Keon <lune0410k@naver.com>
2024-11-03 20:16:00 +0900

    docs: improve java doc

--- src/main/java/com/curateme/claco/review/domain/vo/ImageUrlVO.java
+++ src/main/java/com/curateme/claco/review/domain/vo/ImageUrlVO.java
@@ -9,12 +9,22 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author      : 이 건
+ * @date        : 2024.11.03
+ * @author devkeon(devkeon123@gmail.com)
+ * ===========================================================
+ * DATE               AUTHOR        NOTE
+ * -----------------------------------------------------------
+ * 2024.11.03		   이 건		   최초 생성
+ */
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ImageUrlVO {
 
+	// 이미지 주소
 	private String imageUrl;
 
 	public static ImageUrlVO fromReviewImage(ReviewImage image) {
