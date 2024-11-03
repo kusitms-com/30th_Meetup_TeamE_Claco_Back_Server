730723f05caf98af9604685aa92dc026e6c5e9ab
Keon <lune0410k@naver.com>
2024-11-03 21:13:50 +0900

    feat: add transactional annotation

--- src/main/java/com/curateme/claco/review/service/PlaceCategoryServiceImpl.java
+++ src/main/java/com/curateme/claco/review/service/PlaceCategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.curateme.claco.review.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.curateme.claco.review.domain.vo.PlaceCategoryVO;
 import com.curateme.claco.review.repository.PlaceCategoryRepository;
@@ -10,6 +11,7 @@ import com.curateme.claco.review.repository.PlaceCategoryRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PlaceCategoryServiceImpl implements PlaceCategoryService{
 
