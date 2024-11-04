6ea21b864ac4afe5c1d19c5d419dd057e492cc16
Keon <lune0410k@naver.com>
2024-11-05 03:06:34 +0900

    docs: add swagger docs

--- src/main/java/com/curateme/claco/preference/domain/vo/RegionPreferenceVO.java
+++ src/main/java/com/curateme/claco/preference/domain/vo/RegionPreferenceVO.java
@@ -2,6 +2,7 @@ package com.curateme.claco.preference.domain.vo;
 
 import com.curateme.claco.preference.domain.entity.RegionPreference;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RegionPreferenceVO {
 
+	@Schema(description = "수정하고자 하는 지역 이름", example = "서울")
 	private String preferenceRegion;
 
 	public static RegionPreferenceVO fromEntity(RegionPreference regionPreference) {
--- src/main/java/com/curateme/claco/preference/domain/vo/TypePreferenceVO.java
+++ src/main/java/com/curateme/claco/preference/domain/vo/TypePreferenceVO.java
@@ -2,6 +2,7 @@ package com.curateme.claco.preference.domain.vo;
 
 import com.curateme.claco.preference.domain.entity.TypePreference;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TypePreferenceVO {
 
+	@Schema(description = "수정하고자 하는 유형 내용", example = "깊이 있는 클래식 공연이 좋아요.")
 	private String preferenceType;
 
 	public static TypePreferenceVO fromEntity(TypePreference typePreference) {
