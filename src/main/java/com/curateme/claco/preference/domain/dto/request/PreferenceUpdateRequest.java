1c29fe78619ed7bd0f206b09f5db3fd00208a422
Keon <lune0410k@naver.com>
2024-11-05 03:06:47 +0900

    docs: add swagger docs

--- src/main/java/com/curateme/claco/preference/domain/dto/request/PreferenceUpdateRequest.java
+++ src/main/java/com/curateme/claco/preference/domain/dto/request/PreferenceUpdateRequest.java
@@ -6,6 +6,7 @@ import com.curateme.claco.member.domain.entity.Gender;
 import com.curateme.claco.preference.domain.vo.RegionPreferenceVO;
 import com.curateme.claco.preference.domain.vo.TypePreferenceVO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,11 +28,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PreferenceUpdateRequest {
 
+	@Schema(description = "수정하고자 하는 성별, 수정 없으면 null", example = "MALE / FEMALE")
 	private Gender gender;
+	@Schema(description = "수정하고자 하는 나이, 수정 없으면 null", example = "26")
 	private Integer age;
+	@Schema(description = "수정하고자 하는 최소 가격, 수정 없으면 null", example = "1000")
 	private Integer minPrice;
+	@Schema(description = "수정하고자 하는 최대 가격, 수정 없으면 null", example = "1000000")
 	private Integer maxPrice;
+	@Schema(description = "수정하고자 하는 지역 선호도, 새롭게 추가된 데이터 + 유지된 데이터", example = "서울, 강원 -> 서울, 경기")
 	private List<RegionPreferenceVO> regionPreferences;
+	@Schema(description = "수정하고자 하는 공연 유형, 새롭게 추가된 데이터 + 유지된 데이터", example = "어쩌구, 저쩌구 -> 어쩌구")
 	private List<TypePreferenceVO> typePreferences;
 
 }
