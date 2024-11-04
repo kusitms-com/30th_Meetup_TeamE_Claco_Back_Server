01949bc71df4ac2216de9609abf071afbe059f4e
Keon <lune0410k@naver.com>
2024-11-05 01:26:35 +0900

    feat: add preference read, update method

--- src/main/java/com/curateme/claco/preference/service/PreferenceService.java
+++ src/main/java/com/curateme/claco/preference/service/PreferenceService.java
@@ -1,6 +1,8 @@
 package com.curateme.claco.preference.service;
 
 import com.curateme.claco.member.domain.dto.request.SignUpRequest;
+import com.curateme.claco.preference.domain.dto.request.PreferenceUpdateRequest;
+import com.curateme.claco.preference.domain.dto.response.PreferenceInfoResponse;
 import com.curateme.claco.preference.domain.entity.Preference;
 
 /**
@@ -11,6 +13,7 @@ import com.curateme.claco.preference.domain.entity.Preference;
  * DATE               AUTHOR        NOTE
  * -----------------------------------------------------------
  * 2024.10.22   	   이 건        최초 생성
+ * 2024.11.05   	   이 건        취향 정보 조회, 수정 메서드 추가
  */
 public interface PreferenceService {
 
@@ -20,5 +23,18 @@ public interface PreferenceService {
 	 * @return : 저장한 Preference 반환 (Type, Region 제외)
 	 */
 	Preference savePreference(SignUpRequest signUpRequest);
+
+	/**
+	 * 접근한 유저의 취향 정보를 종합해서 조회
+	 * @return : 유저 취향 정보 데이터
+	 */
+	PreferenceInfoResponse readPreference();
+
+	/**
+	 * 접근한 유저의 취향 정보를 수정
+	 * @param request : 수정할 취향 정보들
+	 * @return : 유저 취향 정보 데이터 (수정 후)
+	 */
+	PreferenceInfoResponse updatePreference(PreferenceUpdateRequest request);
 
 }
