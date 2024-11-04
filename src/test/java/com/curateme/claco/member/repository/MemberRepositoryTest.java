9bfa88d1c16f554360e00b4a0e56ad4b54bdcd71
Keon <lune0410k@naver.com>
2024-11-05 01:24:00 +0900

    test: add new method test

--- src/test/java/com/curateme/claco/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/curateme/claco/member/repository/MemberRepositoryTest.java
@@ -13,6 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.curateme.claco.clacobook.domain.entity.ClacoBook;
 import com.curateme.claco.member.domain.entity.Member;
 import com.curateme.claco.member.domain.entity.Role;
+import com.curateme.claco.preference.domain.entity.Preference;
+import com.curateme.claco.preference.domain.entity.RegionPreference;
+import com.curateme.claco.preference.domain.entity.TypePreference;
 
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
  * DATE               AUTHOR        NOTE
  * -----------------------------------------------------------
  * 2024.10.18   	   이 건        최초 생성
+ * 2024.11.05   	   이 건        추가된 메서드 테스트 추가
  */
 @Slf4j
 @Transactional
@@ -130,5 +134,49 @@ class MemberRepositoryTest {
 		assertThat(result.get().getClacoBooks().size()).isEqualTo(2);
 
 	}
+
+	@Test
+	@DisplayName("취향 정보 함께 조회")
+	void findMemberWithPreference() {
+		// Given
+		Member testMember = Member.builder()
+			.email("test@test.com")
+			.nickname(testString)
+			.role(testRole)
+			.socialId(testLong)
+			.profileImage(testString)
+			.build();
+		entityManager.persist(testMember);
+
+		Preference preference = Preference.builder()
+			.member(testMember)
+			.build();
+		entityManager.persist(preference);
+		testMember.updatePreference(preference);
+
+		RegionPreference regionPreference = RegionPreference.builder()
+			.preference(preference)
+			.regionName(testString)
+			.build();
+		entityManager.persist(regionPreference);
+		preference.addRegionPreference(regionPreference);
+		TypePreference typePreference = TypePreference.builder()
+			.preference(preference)
+			.typeContent(testString)
+			.build();
+		entityManager.persist(typePreference);
+		preference.addTypeReference(typePreference);
+
+		// When
+		Optional<Member> result = memberRepository.findMemberByIdIs(testMember.getId());
+
+		// Then
+		assertThat(result.isPresent()).isTrue();
+		assertThat(result.get().getPreference()).isEqualTo(preference);
+		assertThat(result.get().getPreference().getRegionPreferences()).contains(regionPreference);
+		assertThat(result.get().getPreference().getTypePreferences()).contains(typePreference);
+
+	}
+
 
 }
