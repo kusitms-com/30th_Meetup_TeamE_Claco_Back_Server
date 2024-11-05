cdff80083c65b73781e7974aff2180e8696232b3
Keon <lune0410k@naver.com>
2024-11-05 20:13:47 +0900

    docs: add swagger annotation

--- src/main/java/com/curateme/claco/member/domain/dto/response/MemberInfoResponse.java
+++ src/main/java/com/curateme/claco/member/domain/dto/response/MemberInfoResponse.java
@@ -2,6 +2,7 @@ package com.curateme.claco.member.domain.dto.response;
 
 import com.curateme.claco.member.domain.entity.Member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,8 +25,10 @@ import lombok.NoArgsConstructor;
 public class MemberInfoResponse {
 
 	// 닉네임
+	@Schema(description = "회원 설정 닉네임", example = "클라코사용자")
 	private String nickname;
 	// 이미지 Url
+	@Schema(description = "회원 프사 url", example = "https://claco-defaul/default.png")
 	private String imageUrl;
 
 	public static MemberInfoResponse fromEntity(Member member) {
