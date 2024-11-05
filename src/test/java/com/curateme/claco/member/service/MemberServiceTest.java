132f2531ffbe25c59ef4337a95102c6222b4d498
Keon <lune0410k@naver.com>
2024-11-05 19:27:57 +0900

    test: add member service test for read&update

--- src/test/java/com/curateme/claco/member/service/MemberServiceTest.java
+++ src/test/java/com/curateme/claco/member/service/MemberServiceTest.java
@@ -3,6 +3,7 @@ package com.curateme.claco.member.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,12 +14,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.curateme.claco.authentication.domain.JwtMemberDetail;
 import com.curateme.claco.authentication.util.SecurityContextUtil;
 import com.curateme.claco.global.exception.BusinessException;
 import com.curateme.claco.global.response.ApiStatus;
+import com.curateme.claco.global.util.S3Util;
 import com.curateme.claco.member.domain.dto.request.SignUpRequest;
+import com.curateme.claco.member.domain.dto.response.MemberInfoResponse;
 import com.curateme.claco.member.domain.entity.Gender;
 import com.curateme.claco.member.domain.entity.Member;
 import com.curateme.claco.member.domain.entity.Role;
@@ -39,6 +43,7 @@ import lombok.extern.slf4j.Slf4j;
  * DATE               AUTHOR        NOTE
  * -----------------------------------------------------------
  * 2024.10.22   	   이 건        최초 생성
+ * 2024.11.05   	   이 건        추가 메서드에 대한 테스트 코드 생성
  */
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -50,6 +55,8 @@ class MemberServiceTest {
 	private SecurityContextUtil securityContextUtil;
 	@Mock
 	private PreferenceService preferenceService;
+	@Mock
+	private S3Util s3Util;
 	@InjectMocks
 	private MemberServiceV1 memberService;
 
@@ -136,4 +143,76 @@ class MemberServiceTest {
 		assertThat(testMember.getRole()).isEqualTo(Role.MEMBER);
 
 	}
+
+	@Test
+	@DisplayName("회원 정보 조회")
+	void readMemberInfo() {
+		// Given
+		JwtMemberDetail jwtMemberDetailMock = mock(JwtMemberDetail.class);
+
+		Member testMember = Member.builder()
+			.id(testLong)
+			.email(testString)
+			.nickname(testString)
+			.profileImage(testString)
+			.socialId(testLong)
+			.role(Role.SOCIAL)
+			.build();
+
+		when(securityContextUtil.getContextMemberInfo()).thenReturn(jwtMemberDetailMock);
+		when(jwtMemberDetailMock.getMemberId()).thenReturn(testLong);
+		when(memberRepository.findById(testLong)).thenReturn(Optional.of(testMember));
+
+		// When
+		MemberInfoResponse result = memberService.readMemberInfo();
+
+		// Then
+		verify(securityContextUtil).getContextMemberInfo();
+		verify(jwtMemberDetailMock).getMemberId();
+		verify(memberRepository).findById(testLong);
+
+		assertThat(result.getNickname()).isEqualTo(testString);
+		assertThat(result.getImageUrl()).isEqualTo(testString);
+
+	}
+
+	@Test
+	@DisplayName("회원 정보 업데이트")
+	void updateMemberInfo() throws IOException {
+		// Given
+		JwtMemberDetail jwtMemberDetailMock = mock(JwtMemberDetail.class);
+		String updateString = "update";
+		String testUrlString = "test/url";
+		MultipartFile mockFile = mock(MultipartFile.class);
+
+		Member testMember = Member.builder()
+			.id(testLong)
+			.email(testString)
+			.nickname(testString)
+			.profileImage(testString)
+			.socialId(testLong)
+			.role(Role.SOCIAL)
+			.build();
+
+		when(securityContextUtil.getContextMemberInfo()).thenReturn(jwtMemberDetailMock);
+		when(jwtMemberDetailMock.getMemberId()).thenReturn(testLong);
+		when(memberRepository.findById(testLong)).thenReturn(Optional.of(testMember));
+		when(s3Util.uploadImage(any(MultipartFile.class), any(String.class))).thenReturn(testUrlString);
+
+		// When
+		MemberInfoResponse result = memberService.updateMemberInfo(updateString, mockFile);
+
+		// Then
+		verify(securityContextUtil).getContextMemberInfo();
+		verify(jwtMemberDetailMock).getMemberId();
+		verify(memberRepository).findById(testLong);
+		verify(s3Util).uploadImage(any(MultipartFile.class), any(String.class));
+
+		assertThat(result.getNickname()).isEqualTo(updateString);
+		assertThat(result.getImageUrl()).isEqualTo(testUrlString);
+		assertThat(testMember.getNickname()).isEqualTo(updateString);
+		assertThat(testMember.getProfileImage()).isEqualTo(testUrlString);
+
+	}
+
 }
