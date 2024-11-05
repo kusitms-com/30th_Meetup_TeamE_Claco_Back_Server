5092407d3c3fb1b3f0058482092a7b195f73e085
Keon <lune0410k@naver.com>
2024-11-05 21:42:34 +0900

    feat: add category list response dto

---
+++ src/main/java/com/curateme/claco/review/domain/dto/response/CategoryListResponse.java
@@ -0,0 +1,27 @@
+package com.curateme.claco.review.domain.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : 이 건
+ * @date : 2024.11.05
+ * @author devkeon(devkeon123 @ gmail.com)
+ * ===========================================================
+ * DATE               AUTHOR        NOTE
+ * -----------------------------------------------------------
+ * 2024.11.05   	   이 건        최초 생성
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CategoryListResponse<T> {
+
+	private T categories;
+
+}
