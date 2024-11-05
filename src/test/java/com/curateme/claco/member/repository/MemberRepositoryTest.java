fbe9a1299d8a77cb6c742cf6c8b3a7d466c1f3a2
Keon <lune0410k@naver.com>
2024-11-06 00:31:49 +0900

    chore: apply test container

--- src/test/java/com/curateme/claco/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/curateme/claco/member/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,6 +34,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Transactional
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberRepositoryTest {
 
 	@Autowired
