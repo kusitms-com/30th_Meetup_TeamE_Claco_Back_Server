dea869a08684218d58c13497fd686db99e5ffd8d
정희찬 <anselmo228@naver.com>
2024-11-07 23:31:15 +0900

    feature: Swagger Operation 추가

--- src/main/java/com/curateme/claco/recommendation/controller/RecommendationController.java
+++ src/main/java/com/curateme/claco/recommendation/controller/RecommendationController.java
@@ -3,6 +3,7 @@ package com.curateme.claco.recommendation.controller;
 import com.curateme.claco.global.response.ApiResponse;
 import com.curateme.claco.recommendation.domain.dto.RecommendationConcertsResponse;
 import com.curateme.claco.recommendation.service.RecommendationService;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class RecommendationController {
     private final RecommendationService recommendationService;
 
     @GetMapping
+    @Operation(summary = "나의 취향 기반 맞춤 추천", description = "기능명세서 화면번호 2.0.0(C)")
     public ApiResponse<List<RecommendationConcertsResponse>> getConcertRecommendations(
         @RequestParam Long userId
     ){
