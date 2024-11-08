4b88f92849a6654077a789a46dd5527580f99b04
정희찬 <anselmo228@naver.com>
2024-11-08 20:19:51 +0900

    feature: DTO Schema 추가

--- src/main/java/com/curateme/claco/review/domain/dto/response/TicketReviewSummaryResponse.java
+++ src/main/java/com/curateme/claco/review/domain/dto/response/TicketReviewSummaryResponse.java
@@ -1,12 +1,19 @@
 package com.curateme.claco.review.domain.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
 public class TicketReviewSummaryResponse {
+
+    @Schema(name = "concert_id")
     private Long concertId;
+
+    @Schema(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Schema(name = "review_content")
     private String content;
 
     // Constructor
