e57e420c659d746df8a41911146ec0330ef9a95f
정희찬 <anselmo228@naver.com>
2024-11-15 22:52:26 +0900

    refactoring: 둘러보기 날짜 필터링 쿼리 수정

--- src/main/java/com/curateme/claco/concert/repository/ConcertRepository.java
+++ src/main/java/com/curateme/claco/concert/repository/ConcertRepository.java
@@ -12,7 +12,7 @@ import org.springframework.data.repository.query.Param;
 public interface ConcertRepository extends JpaRepository<Concert,Long> {
     Page<Concert> findByIdIn(List<Long> ids, Pageable pageable);
 
-    @Query("SELECT c.id FROM Concert c " + "WHERE c.area = :area " + "AND c.prfpdfrom BETWEEN :startDate AND :endDate")
+    @Query("SELECT c.id FROM Concert c " + "WHERE c.area = :area " + "AND c.prfpdto BETWEEN :startDate AND :endDate")
     List<Long> findConcertIdsByFilters(@Param("area") String area, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
     @Query("SELECT c.id FROM Concert c " + "WHERE (c.prfnm LIKE %:query% " + "OR c.prfcast LIKE %:query% " + "OR c.fcltynm LIKE %:query%)")
